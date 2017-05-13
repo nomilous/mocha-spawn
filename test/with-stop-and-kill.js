b8735f8d5deb1d3f5254204c375e1b6a17d8e16b
nomilous <nomilous@gmail.com>
2017-05-13 15:06:43 +0200

    with stop and kill

---
+++ test/with-stop-and-kill.js
@@ -0,0 +1,33 @@
+var MochaFork = require('..');
+var path = require('path');
+var fetchUrl = require('fetch').fetchUrl;
+var expect = require('expect.js');
+
+describe('with stop', function () {
+
+  var scriptFile = path.resolve(__dirname, 'procs', 'http-server-with-stop');
+  var scriptOpts = {
+    port: 8080,
+    host: 'localhost'
+  };
+
+  MochaFork.start(scriptFile, scriptOpts);
+
+  MochaFork.stop(scriptFile);
+
+  MochaFork.kill(scriptFile);
+
+  it('started server', function (done) {
+
+    fetchUrl('http://localhost:8080', function (e, meta, body) {
+
+      if (e) return done(e);
+
+      expect(body.toString()).to.be('OK');
+      done();
+
+    });
+
+  });
+
+});

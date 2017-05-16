c531d25b8c5ba85acc970ff8c7a87b4bb339d62a
nomilous <nomilous@gmail.com>
2017-05-16 11:05:41 +0200

    2.0.1

--- RELEASES.md
+++ RELEASES.md
@@ -7,7 +7,7 @@
 
 - updated readme
 
-## 2.0.0 - 
+## 2.0.0 - 14 May 2017
 
 - added unit tests to improve coverage where in child process
 - removed after and afterEach
@@ -16,3 +16,7 @@
 - added opts to stop and kill
 - added opts.timeout
 - send to child with multiple event args
+
+## 2.0.1 - 16 May 2017
+
+- kill all children on parent exit
---
+++ test/with-no-die.js
@@ -0,0 +1,18 @@
+var MochaFork = require('..');
+var path = require('path');
+
+describe('with no die', function () {
+
+  var scriptFile = path.resolve(__dirname, 'procs', 'server-with-no-die');
+
+  var childRef = MochaFork.before.start(scriptFile);
+
+  // childRef.after.stop();
+
+  it('started and stopped', function () {
+
+    // manual check child exited on mocha exited
+
+  });
+
+});

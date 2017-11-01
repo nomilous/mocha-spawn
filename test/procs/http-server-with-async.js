422a3f0465dc27d84b18fb459fd7a6191fd934f5
nomilous <nomilous@gmail.com>
2017-11-01 20:50:35 +0200

    rename

--- test/procs/http-server-with-async.js
+++ test/procs/http-server-with-async.js
@@ -1,17 +1,17 @@
-const MochaSpawn = require('../..');
+const mochaSpawn = require('../..');
 const HttpServer = require('./http-server-promise');
 
 // var server = new HttpServer();
 var server;
 
-MochaSpawn.onStart(async function (opts) {
+mochaSpawn.onStart(async function (opts) {
 
   // await server.start(opts);
   server = await HttpServer.create(opts);
 
 });
 
-MochaSpawn.onStop(async function () {
+mochaSpawn.onStop(async function () {
 
   console.log('STOP 1');
 
